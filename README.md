3a0840611f19c05052101228f81eaac12a98f297
Jaytak <jaquangayboi@gmail.com>
2024-02-10 16:13:28 +1300

    Update V1.0.0 Minor Bug Fix, Cleaned up Code. First Release.

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # JayTAK Repair Plugin
 
-Version 0.0.27-BETA
+Version 1.0.0
+
 
 Only tested on paper 1.20.4 build 409
 
-TODO....
+Simple minecraft repair plugin that allows players to repair the item they are holding in their hands for the price of the base material the item is made from.
+##### Commands & Aliases:
+```
+/jaytakrepair
+/jrepair
+/jrep
+/jr
+```
+##### Permission Nodes:
+``
+jaytakrepairplugin.repair
+``
+Grants the player access to the command.
+
+##### Changelog:<br>
+V0.0.1-ALPHA - V0.0.20-ALPHA - Getting the plugin to function.<br>
+V0.0.21-ALPHA - V0.0.28-BETA - Removing essentials as a dependency.<br>
+V0.0.29-BETA - Added Permission Node For Command.<br>
+V1.0.0 - Minor bug fix, cleaned up the code a bit.<br>
--- src/main/java/io/jaytak/jaytakrepairplugin/Repair.java
+++ src/main/java/io/jaytak/jaytakrepairplugin/Repair.java
@@ -22,14 +22,14 @@ public class Repair implements CommandExecutor {
         if (!(sender instanceof Player)) {
             player.sendMessage("[JayTAK Repair] Only players can use this command!");
             getLogger().info("[JayTAK Repair] Only players can use this command!");
-            return false;
+            return false; // Command did not execute successfully.
         }
         // Check player has permission to use the command.
         getLogger().info("[JayTAK Repair] " + sender.getName() + " issued the command " + command.getName());
         if (!player.hasPermission("jaytakrepairplugin.repair")){
             getLogger().info("[JayTAK Repair} " + sender.getName() + " doesnt have permission to run this command.");
             player.sendMessage("[JayTAK Repair] You dont have permission to run this command.");
-            return false;
+            return false; // Command did not execute successfully.
         }
 
 
@@ -50,30 +50,36 @@ public class Repair implements CommandExecutor {
                         player.setItemInHand(new ItemStack(Material.ANVIL, 1));
                         player.sendMessage("[JayTAK Repair] Item repaired using one Iron Block.");
                         getLogger().info("[JayTAK Repair] Item repaired using one Iron Block.");
+                        // Update players inventory.
                         player.updateInventory();
-                        return true;
+                        return true;  // Command executed successfully.
                     }
                     else {
                         player.sendMessage("[JayTAK Repair] You don't have enough Iron Blocks to repair " + itemInHand.getType());
                         getLogger().info("[JayTAK Repair] Player " + username + " doesnt have enough Iron Blocks to repair " + itemInHand.getType());
-                        return false;
+                        return false; // Command did not execute successfully.
                     }
                 }
 
                 // Repairable/damageable Items:
+
+                // Get items MetaData, then check if the MetaData is type Damageable.
                 ItemMeta data = itemInHand.getItemMeta();
                 if (data instanceof Damageable){
-                    Damageable damageable = (Damageable) data;
-                    int damageValue = damageable.getDamage();
+                    Damageable damageable = (Damageable) data; // Cast data as Damageable
+                    int damageValue = damageable.getDamage(); // Store items current damage value
                     getLogger().info("[JayTAK Repair] Item Damage Value Is: " + damageValue);
-                    if (damageValue > 0) {
+                    if (damageValue > 0) { // If item has damage.
                         getLogger().info("[JayTAK Repair] Attempting to repair item.");
 
 
-                        String[] parts = item.split("_");
+                        String[] parts = item.split("_");// Split the item name by _
 
                         String itemMaterial = parts[0];
+
                         switch(itemMaterial){
+
+                            // Items to repair using OAK_PLANKS
                             case "FISHING":
                             case "BOW":
                             case "CROSSBOW":
@@ -94,6 +100,7 @@ public class Repair implements CommandExecutor {
                                 }
                                 break;
 
+                            // Items to repair using COBBLESTONE
                             case "STONE":
                                 if (player.getInventory().contains(Material.COBBLESTONE)) {
                                     // Remove one item of the material from the player's inventory
@@ -109,6 +116,8 @@ public class Repair implements CommandExecutor {
                                     getLogger().info("[JayTAK Repair] Player " + username + " doesnt have enough Cobblestone to repair " + itemInHand.getType());
                                 }
                                 break;
+
+                            // Items to repair using IRON_INGOTS
                             case "SHIELD":
                             case "IRON":
                                 if (player.getInventory().contains(Material.IRON_INGOT)) {
@@ -126,6 +135,7 @@ public class Repair implements CommandExecutor {
                                 }
                                 break;
 
+                            // Items to repair using GOLD_INGOTS
                             case "GOLD":
                                 if (player.getInventory().contains(Material.GOLD_INGOT)) {
                                     // Remove one item of the material from the player's inventory
@@ -142,6 +152,7 @@ public class Repair implements CommandExecutor {
                                 }
                                 break;
 
+                            // Items to repair using DIAMONDS
                             case "DIAMOND":
                                 if (player.getInventory().contains(Material.DIAMOND)) {
                                     // Remove one item of the material from the player's inventory
@@ -157,6 +168,8 @@ public class Repair implements CommandExecutor {
                                     getLogger().info("[JayTAK Repair] Player " + username + " doesnt have enough Diamonds to repair " + itemInHand.getType());
                                 }
                                 break;
+
+                            // Items to repair using NETHERITE_INGOTS
                             case "TRIDENT":
                             case "ELYTRA":
                             case "NETHERITE":
@@ -174,35 +187,42 @@ public class Repair implements CommandExecutor {
                                     getLogger().info("[JayTAK Repair] Player " + username + " doesnt have enough Netherite to repair " + itemInHand.getType());
                                 }
                                 break;
+
+                            // Any Other Item.
                             default:
                                 player.sendMessage("[JayTAK Repair] Item " + itemMaterial + " currently not supported by the plugin, poke JayTAK...");
                                 getLogger().info("[JayTAK Repair] Item " + itemMaterial + " currently not supported by the plugin, poke JayTAK...");
                                 break;
                         }
 
+                        // Update players inventory.
                         player.updateInventory();
                     }
+                    // Item does not have any damage.
                     else{
                         player.sendMessage("[JayTAK Repair] Item " + itemInHand.getType() + " does not need repairing!");
                         getLogger().info("[JayTAK Repair] Item " + itemInHand.getType() + " does not need repairing!");
                     }
                 }
+                // Item does not have any metaData.
                 else{
                     player.sendMessage("[JayTAK Repair] Item does not have metadata!");
                     getLogger().info("[JayTAK Repair] Item does not have MetaData!");
                 }
             }
+            // Player not holding an item in their hand.
             else {
                 player.sendMessage("[JayTAK Repair] You need to hold an item in your hand to use this command.");
                 getLogger().info("[JayTAK Repair]  Player " + username + " doesnt have an item in their hand.");
             }
-            return true;
+            return true; // Command executed successfully.
         }
+        // Caught Exception
         catch (Exception exception){
             player.sendMessage("[JayTAK Repair] Caught an Exception, contact an admin to check the logs.");
             getLogger().info("[JayTAK Repair] Caught Exception: " + exception);
             //exception.printStackTrace();
         }
-        return false;
+        return false; // Command did not execute successfully.
     }
 }
