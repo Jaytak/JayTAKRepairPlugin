2bc25721d1cb6dba746884599f60a6de4674d65e
Jay <78488517+Jaytak@users.noreply.github.com>
2024-05-03 23:44:36 +1200

    Basic config file setup. Refactored code.

--- src/main/java/io/jaytak/jaytakrepairplugin/JayTAKRepairPlugin.java
+++ src/main/java/io/jaytak/jaytakrepairplugin/JayTAKRepairPlugin.java
@@ -1,15 +1,73 @@
 package io.jaytak.jaytakrepairplugin;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class JayTAKRepairPlugin extends JavaPlugin {
+    public static final Map<String, Material> repairMaterials = new HashMap<>();
+    public static FileConfiguration config;
+    private File configFile;
+
+    private void loadRepairMaterials() {
+        repairMaterials.clear();
+        for (String key : config.getKeys(false)) {
+            try {
+                Material material = Material.valueOf(config.getString(key));
+                repairMaterials.put(key, material);
+            } catch (IllegalArgumentException e) {
+                Logger logger = Logger.getLogger("Minecraft");
+                logger.warning("Invalid material specified for key: " + key);
+            }
+        }
+    }
+
+    private void ConfigLoad(){
+        configFile = new File("plugins/JayTAKRepair/config.yml");
+        boolean fileCreated = false;
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            try {
+                configFile.createNewFile();
+                fileCreated = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+        if (fileCreated){
+            try{
+                initializeDefaultConfig();
+            }
+            catch(Exception ex){
+                getLogger().warning("Jaytak Repair Ran into exception: " + ex.toString());
+            }
+        }
+        loadRepairMaterials();
+    }
+
+    private void initializeDefaultConfig() throws IOException {
+        config.options().copyDefaults(true);
+        config.addDefault("repairMaterials.DAMAGED_ANVIL", Material.IRON_BLOCK.name());
+        config.addDefault("repairMaterials.CHIPPED_ANVIL", Material.IRON_INGOT.name());
+        config.addDefault("repairMaterials.DIAMOND_PICKAXE", Material.DIAMOND.name());
+
+        config.save(configFile);
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         int pluginId = 20963;
         metrics metrics = new metrics(this, pluginId);
-
+        ConfigLoad();
         super.onEnable();
         Objects.requireNonNull(getCommand("jaytakrepair")).setExecutor(new Repair());
         this.getLogger().info("JayTAK Repair Plugin Loaded");
